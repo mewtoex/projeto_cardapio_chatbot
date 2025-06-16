cc7b75008f989b6583c9ae33e937416c3a14cc2b
Whanderson <whandersonba75@gmail.com>
2025-06-16 01:15:55 -0300

    Removido back python

--- backend/src/api/serializers/category_schema.py
+++
@@ -1,9 +0,0 @@
-from marshmallow import Schema, fields, validate
-
-class CategorySchema(Schema):
-    id = fields.Integer(dump_only=True)
-    name = fields.String(required=True, validate=validate.Length(min=1, max=80))
-    description = fields.String(validate=validate.Length(max=200), allow_none=True)
-
-category_schema = CategorySchema()
-categories_schema = CategorySchema(many=True)
